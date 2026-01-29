38f31e3b1d24d2a439c663ca525a90c5071c5696
ubleck <justinmarguijo1@gmail.com>
2026-01-29 00:48:49 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,24 @@
+# 10demo.py by Justin Arguijo
+
+import math
+
+print('hello, again') # greeting
+"""
+This is a
+multi-line
+comment
+"""
+
+print(1.5e-2)
+print(1+1)
+print(1-1)
+print(2*2)
+print(1/2)
+print(2**3)
+print(5//2)
+print(5%2)
+print(5*(2+1))
+
+print(math.pow(2,3))
+print(math.sqrt(2))
+print(math.log(2))
