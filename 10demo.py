a64497842ba096de59b3e4c6a231b22e0ff9c46f
ubleck <justinmarguijo1@gmail.com>
2026-02-13 04:39:10 -0800

    another

--- 10demo.py
+++ 10demo.py
@@ -19,6 +19,12 @@ print(5//2)
 print(5%2)
 print(5*(2+1))
 
+print(abs(21))
+print(pow(2, 3)
+print(round(12345, ndigits=3)
+
 print(math.pow(2,3))
 print(math.sqrt(2))
 print(math.log(2))
+print('ascii values:')
+print('A = 65, a = 97')
