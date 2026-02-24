6ef572bc08f040ffffdd78391ed3caa255b739b4
ubleck <justinmarguijo1@gmail.com>
2026-02-23 17:11:35 -0800

    workinggg

--- 10demo.py
+++ 10demo.py
@@ -51,3 +51,76 @@ def mph_to_kph(s): return s * 1.609
 print(mph_to_kph(10))
 def arith_mean(x, y, z): return (x + y + z) / 3
 print(arith_mean(3, 3, 3))
+
+c = 2
+d = 2
+if c == d:
+    print('c equals d')
+print(c, d)
+
+def is_even(x):
+    if x % 2 == 0: return True
+    return False
+print(is_even(2))
+print(is_even(3))
+
+e = c == d
+print(e)
+print(type(e))
+
+if   a < b: print('a < b')
+elif a > b: print('a > b')
+else:       print('a == b')
+if a < b or a > b: print('all things being equal, a and b are not')
+if a < b and a > b: print('you aer living in a strange world')
+if not False: print(True)
+
+a = 0.3
+b = 0.1 * 3
+if   a < b: print('a < b')
+elif a > b: print('a > b')
+else:       print('a == b')
+print(abs(a-b)) #5.551115123125783e-17
+if abs(a-b) < 1e-9: print('close enough')
+if math.isclose(a, b): print('close enough')
+
+s1 = 'A'
+s2 = 'B'
+s3 = 'a'
+if s1 < s2: print('A < B')
+if s2 < s3: print('B < a')
+ 
+print('Hi')
+
+x = 10
+def is_integer(x):
+    if x % 2 == 0: print('is integer')
+    else:          print('not integer')
+
+is_integer(x)
+
+print(x)
+
+g = 1.000000000001
+def is_validp(g):
+    if g <= 1: print(g, 'is valid probability')
+    else:      print(g, ' is not a valid probability')
+is_validp(g)
+
+w = 'U'
+def dnaweight(w):
+    if   w == 'A': print('331.22g/mol')
+    elif w == 'C': print('307.19g/mol')
+    elif w == 'G': print('347.22g/mol')
+    elif w == 'T': print('322.21g/mol')
+    else: print('None')
+dnaweight(w)
+
+c = 'U'
+def dnacomp(c):
+    if   c == 'A': print('T')
+    elif c == 'C': print('G')
+    elif c == 'G': print('C')
+    elif c == 'T': print('A')
+    else:          print('None')
+dnacomp(c)    
